5c751423c42da0255a3d3e44389a028ae8f1c2a5
Wandy <wandy20number@gmail.com>
2017-08-17 23:38:27 +0700

    Fragment & RecycleView

---
+++ MovieManager/app/src/main/java/wansu/example/com/moviemanager/fragment/NowPlayingFragment.java
@@ -0,0 +1,30 @@
+package wansu.example.com.moviemanager.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import wansu.example.com.moviemanager.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NowPlayingFragment extends Fragment {
+
+
+    public NowPlayingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_now_playing, container, false);
+    }
+
+}
